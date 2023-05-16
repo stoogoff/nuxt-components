6de062952d2055fb2a9a6c317289ecf411698687
Stoo <stoo.goff@gmail.comn>
2023-05-16 18:57:18 +0100

    Updated component loader to include sub directories

--- nuxt.js
+++ nuxt.js
@@ -1,10 +1,22 @@
 import { join } from 'path'
 
+const components = [
+	'components',
+	'components/buttons',
+	'components/files',
+	'components/forms',
+	'components/nav',
+	'components/tabs',
+	'components/transitions',
+]
+
 export default function() {
 	this.nuxt.hook('components:dirs', dirs => {
-		dirs.push({
-			path: join(__dirname, 'components'),
-			prefix: 'we',
+		componens.forEach(comp => {
+			dirs.push({
+				path: join(__dirname, comp),
+				prefix: 'we',
+			})
 		})
 	})
 }
